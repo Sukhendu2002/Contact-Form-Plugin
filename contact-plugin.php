059f75b884be9f3575eab1dd388aea14aee482b1
Sukhendu Sekhar Guria <sukhenduguria2017@gmail.com>
2024-01-02 17:54:52 +0530

    Initial Plugin Files

---
+++ contact-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Contact Plugin
+ *
+ * @package ContactPlugin
+ */
+
+/**
+ * Plugin Name: Contact Plugin
+ * Description: A plugin to create contact forms.
+ * Version: 1.0.0
+ * Text Domain: contact-plugin
+ */
