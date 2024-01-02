634f7c9609d4bd0e051896c8e21c098dd5636b86
Sukhendu Sekhar Guria <sukhenduguria2017@gmail.com>
2024-01-02 20:04:59 +0530

    Set up REST Endpoint to Receive Post Requests

--- includes/contact-form.php
+++ includes/contact-form.php
@@ -6,7 +6,37 @@
  */
 
 add_shortcode( 'contact', 'show_contact_form' );
-function show_contact_form() {
+/**
+ * Function to show contact form
+ *
+ * @return string
+ */
+function show_contact_form(): string {
 	$contact_form = file_get_contents( MY_PLUGIN_PATH . '/includes/templates/contact-form.php', true );
 	return $contact_form;
+}
+
+/**
+ * Function to create rest route
+ *
+ * @return void
+ */
+function create_rest_route(): void {
+	register_rest_route(
+		'contact-plugin/v1',
+		'/contact',
+		array(
+			'methods'  => 'POST',
+			'callback' => 'contact_form_submit',
+		)
+	);
+}
+
+/**
+ * Function to submit contact form
+ *
+ * @return void
+ */
+function contact_form_submit(): void {
+	echo 'Hello';
 }
--- includes/templates/contact-form.php
+++ includes/templates/contact-form.php
@@ -1,22 +1,30 @@
+<?php
+/**
+ * Form Template
+ *
+ * @package ContactPlugin
+ */
+
+?>
 <form>
-    <div class="form-group">
-        <label for="name">Name</label>
-        <input type="text" name="name" id="name" />
-    </div>
+	<div class="form-group">
+		<label for="name">Name</label>
+		<input type="text" name="name" id="name" />
+	</div>
 
-    <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" />
-    </div>
+	<div class="form-group">
+		<label for="email">Email</label>
+		<input type="email" name="email" id="email" />
+	</div>
 
-    <div class="form-group">
-        <label for="phone">Phone</label>
-        <input type="tel" name="phone" id="phone" />
-    </div>
+	<div class="form-group">
+		<label for="phone">Phone</label>
+		<input type="tel" name="phone" id="phone" />
+	</div>
 
-    <div class="form-group">
-        <label for="message">Message</label>
-        <textarea name="message" id="message"></textarea>
-    </div>
-    <button type="submit">Submit</button>
+	<div class="form-group">
+		<label for="message">Message</label>
+		<textarea name="message" id="message"></textarea>
+	</div>
+	<button type="submit">Submit</button>
 </form>
