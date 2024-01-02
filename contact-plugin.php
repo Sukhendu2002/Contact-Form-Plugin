43edfe602c6d369c391a4e8a96d5692d92345e0d
Sukhendu Sekhar Guria <sukhenduguria2017@gmail.com>
2024-01-02 19:02:29 +0530

    Set up Composer Autoload & Constant for Plugin Path

--- contact-plugin.php
+++ contact-plugin.php
@@ -11,3 +11,23 @@
  * Version: 1.0.0
  * Text Domain: contact-plugin
  */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'You are not allowed to call this page directly.' );
+}
+
+if ( ! class_exists( 'ContactPlugin' ) ) {
+	/**
+	 * ContactPlugin class
+	 */
+	class ContactPlugin {
+		/**
+		 * Construct the plugin object
+		 */
+		public function __construct() {
+			define( 'MY_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+			require_once MY_PLUGIN_PATH . '/vendor/autoload.php';
+		}
+	}
+	new ContactPlugin();
+}
